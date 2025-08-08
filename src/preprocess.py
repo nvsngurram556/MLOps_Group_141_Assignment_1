2cb4bf7e9343df4fe5c3b3e08466cc27fcbb2e86
Satya Narayana <nvsngurram@outlook.com>
2025-08-08 16:37:37 +0530

    Part 1 Repository and Data Versioning

---
+++ src/preprocess.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_and_preprocess(csv_path):
+    df = pd.read_csv(csv_path)
+
+    X = df.drop('MedHouseVal', axis=1)
+    y = df['MedHouseVal']
+
+    # Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Scale
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    return X_train_scaled, X_test_scaled, y_train, y_test
